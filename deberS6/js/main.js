30c57707d5540b89de13fb1c4dd1f21cd3d2e777
neo-astro <estevanabril@hotmail.es>
2022-01-23 23:46:12 -0500

    listo

--- deberS6/js/main.js
+++ deberS6/js/main.js
@@ -1,9 +1,9 @@
-
 function mostrar(){
-    
     var nombre = document.getElementById("NOMBRE").value
-    var email = document.getElementById("EMAIL").value
+    var email = document.getElementById("EMAIL").value  
     var mesaje = document.getElementById("MENSAJE").value
-    alert(`Tu nombre es: ${nombre}\ntu email es: ${email}\ntu mesaje es: ${mesaje}`)
+    alert(`Tu nombre es: ${nombre}\ntu correo es: ${email}\ntu mensaje es: ${mesaje}`)
 }
+
+
 
